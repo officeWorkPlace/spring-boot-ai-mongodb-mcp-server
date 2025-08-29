1cf72ee2d8f6e59bb5814bbbef82235fb345c90c
officeWorkPlace <office.place.work.007@gmail.com>
2025-08-30 02:36:34 +0530

    Refactor package name: Main application class from bootcamptoprod to deepai

---
+++ src/main/java/com/deepai/SpringBootAiMongoMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.deepai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiMongoMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAiMongoMcpServerApplication.class, args);
+	}
+
+}
